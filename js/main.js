52321954994406105863ca821e8db0863dfdf1ff
donatimar <donatimar@gmail.com>
2024-10-17 14:37:34 +0200

    comment exercise Mail

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+// *Mail
+/*
+Chiedi all'utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull'esito del controllo.
+*/
 
+// array con lista di email valide
+// prompt di richiesta inserimento email
+// controllo validità dell'email inserita
+// controllo se l'email è nella lista
+// stampo l'esito del controllo
+
+// * Gioco dei dadi
+/*
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+*/
