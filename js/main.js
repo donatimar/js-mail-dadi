32b8a8573aba32f448fb735d70b696c462867e37
donatimar <donatimar@gmail.com>
2024-10-17 14:39:56 +0200

    added valid emails

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull'esito del controllo.
 */
 
+// dichiarazione variabile iniziale
+const validEmails = ["marco@gmail.com", "paolo@gmail.com", "michele@gmail.com"];
+
 // array con lista di email valide
 // prompt di richiesta inserimento email
 // controllo validità dell'email inserita
