e34e4f95a2025343ed486b32408814f4e7913e98
donatimar <donatimar@gmail.com>
2024-10-17 15:17:09 +0200

    commenti exercise dadi

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -3,3 +3,9 @@
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
+
+// genera un numero casuale da 1 a 6 per il giocatore
+
+// genera un numero casuale per il computer
+
+// chi ha fatto il punteggio più alto?
