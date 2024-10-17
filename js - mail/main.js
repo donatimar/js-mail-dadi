8f488ab6107b09702089f06f60b0ecd6a09805b0
donatimar <donatimar@gmail.com>
2024-10-17 15:02:09 +0200

    modified file

--- js - mail/main.js
+++ js - mail/main.js
@@ -35,9 +35,3 @@ if (!userEmail.includes("@") || !userEmail.includes(".")) {
     alert("Accesso negato, l'email inserita non ha diritti di accesso");
   }
 }
-
-// * Gioco dei dadi
-/*
-Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-Stabilire il vincitore, in base a chi fa il punteggio più alto.
-*/
